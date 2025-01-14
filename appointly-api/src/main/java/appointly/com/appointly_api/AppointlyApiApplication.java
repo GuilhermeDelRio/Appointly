dec4bddfa39eabd8183ba33e6e3fa6b93d1b340d
Guilherme Del Rio <guilhermeddelrio@gmail.com>
2025-01-14 18:20:19 -0300

    initial commit

---
+++ appointly-api/src/main/java/appointly/com/appointly_api/AppointlyApiApplication.java
@@ -0,0 +1,13 @@
+package appointly.com.appointly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointlyApiApplication.class, args);
+	}
+
+}
